d42fa8259a7faaa044c818b22673eff0978bad8c
haorany <haorany@kth.se>
2020-02-12 03:46:33 +0800

    Add plot function for Mobing buckets rate per minute

--- algorithm_simulation/stats/03-plot-moving-buckets.py
+++ algorithm_simulation/stats/03-plot-moving-buckets.py
@@ -63,56 +63,57 @@ else:
   set_rcs()
 
 colours = ['b', 'g', 'r', 'c', 'm', 'y', 'v']
+markers = ['+', 'x', 'v', 's', 'd', 'o', '1']
 
-hadoop = []
-webservers = []
-cachefollowers = []
+cheetah = []
+beamer = []
+round_robin = []
+consistent_hashing = []
 
-hd = []
-ws = []
-cf = []
+ct = []
+bm = []
+rr = []
+ch = []
 
-hdmap = {}
-wsmap = {}
-cfmap = {}
+ctmap = {}
+bmmap = {}
+rrmap = {}
+chmap = {}
 
 fname = "report.csv"
 
 filenames = []
 filenames.append(fname)
 
-dict_list = {"hadoop": hdmap}
-# dict_list = {"hadoop": hdmap, "webservers": wsmap, "cachefollowers": cfmap}
-workload = "hadoop"
+dict_list = {"beamer" : bmmap, "round-robin" : rrmap, "least-loaded": ctmap, "consistent-hashing": chmap }
 
 for f in filenames:
-  print "Analyzing file %s: " % (f)
-  for line in open(f).readlines()[1:]:
-    fields = [x.strip() for x in line.split(",")]
-    model = fields[0] #
-    connection_target = fields[1] #
-    end_time = fields[2] #
-    server_number = fields[3] #
-    update_rate = fields[4] #
-    bucket_number = fields[5] #
-    imbalance_threshold = fields[6] #
-    total_connections = fields[7] #
-    broken_connections = fields[8] #
-    broken_rate = fields[9] #
-    max_imbalance = fields[10] #
-    ave_imbalance = fields[11] #
-    moving_bucket_rate = fields[12]
-    if model != "dy_beamer":
-        continue
-    if workload == "hadoop":
-      hdmap.setdefault(imbalance_threshold, [])
-      hdmap[imbalance_threshold].append(moving_bucket_rate)
-    elif workload == "cachefollowers":
-      cfmap.setdefault(imbalance_threshold, [])
-      cfmap[imbalance_threshold].append(moving_bucket_rate)
-    elif workload == "webservers":
-      wsmap.setdefault(imbalance_threshold, [])
-      wsmap[imbalance_threshold].append(moving_bucket_rate)
+    print "Analyzing file %s: " % (f)
+    for line in open(f).readlines()[1:]:
+        fields = [x.strip() for x in line.split(",")]
+        model = fields[0]  #
+        connection_target = fields[1]  #
+        end_time = fields[2]  #
+        server_number = fields[3]  #
+        update_rate = fields[4]  #
+        bucket_number = fields[5]  #
+        imbalance_threshold = fields[6]  #
+        total_connections = fields[7]  #
+        broken_connections = fields[8]  #
+        broken_rate = fields[9]  #
+        max_imbalance = fields[10]  #
+        ave_imbalance = fields[11]  #
+        moving_bucket_rate = fields[12]
+        if model != "dy_beamer":
+            continue
+        if server_number != "468":
+            continue
+        # print(str(connection_target) + " " + str(imbalance) + " " + str(scheme))
+        #connections_per_bucket = int(float(connection_target)/float(servers)/float(buckets_to_server))
+        if model == "dy_beamer":
+            #print("  " +str(connection_target) + " " + str(imbalance))
+            bmmap.setdefault(imbalance_threshold, [])
+            bmmap[imbalance_threshold].append(moving_bucket_rate)
 
 for item in dict_list:
   dict_ = dict_list[item]
@@ -120,32 +121,32 @@ for item in dict_list:
   for key in sorted(dict_.keys()):
     dict_float = [float(x) for x in dict_[key]]
     row = np.average(dict_float)
-    if item == "hadoop":
-      hadoop.append(row)
-      hd.append(key)
-    elif (item == "cachefollowers"):
-      cachefollowers.append(row)
-      cf.append(key)
-    elif (item == "webservers"):
-      webservers.append(row)
-      ws.append(key)
+    if item == "beamer":
+      beamer.append(row)
+      bm.append(key)
 
-plt.xlabel("Imabalance [\%]", fontsize=10)
-plt.ylabel("Moving buckets rate", y=0.32)
-#plt.xscale('log')
+plt.xlabel("Imbalance threshold [\%]",  fontsize=10)
+plt.ylabel("Moving buckets per minute [\%]", y=0.32)
+plt.yscale('log')
 #plt.title('sizeofsequence=16')
 #plt.ylim(0,2000)
-plt.xlim(1.5, 6)
-miny=0
-plt.ylim(miny, 2)
+# plt.xlim(8,42)
+miny=10
+plt.ylim(miny, 1000)
+#plt.yticks(range(0, 5, 1), [str(x) for x in range(0, 5, 1)])
 #plt.yticks(range(miny, 1101, 200), [str(x) for x in range(miny, 1101, 200)])
 
-xaxis = [str((float(i) -1)*100) for i in hd]
-plt.plot(xaxis, hadoop, label="hadoop",color='red', lw=1.0, linestyle='-',marker= '>', mfc='none', mec='red', ms=3)
-# plt.plot(cf, cachefollowers, label="cache-followers",color='blue', lw=1.0, linestyle='-',marker= 's', mfc='none', mec='blue', ms=3)
-# plt.plot(ws, webservers, label="web-servers",color='green', lw=1.0, linestyle='-',marker= 'v', mfc='none', mec='green', ms=3)
+#plt.annotate("45x", (125000, 1.8))
+#plt.annotate("", (120000,0.45), xytext=(120000,16), arrowprops=dict(arrowstyle='<->'))
+#plt.plot(ct, cheetah, label="ideal",color='green', lw=1.0, linestyle='-',marker= 'v', mfc='none', mec='green', ms=3)
+print (bm)
+print (beamer)
+# plt.plot(bm, beamer)
+plt.plot(bm, beamer, label="extended-beamer",color='red', lw=1.0, linestyle='-',marker= '>', mfc='none', mec='red', ms=3)
+#plt.plot(rr, round_robin, label="round-robin",color='blue', lw=1.0, linestyle='-',marker= 's', mfc='none', mec='blue', ms=3)
+#plt.plot(ch, consistent_hashing, label="consistent-hash",color='orange', lw=1.0, linestyle='-',marker= 'x', mfc='none', mec='orange', ms=3)
 
 
-plt.legend(loc='upper left', frameon=False, ncol=2, columnspacing=0.2, handletextpad=0.2)
-plt.savefig("%s.pdf" % outname, format="pdf", bbox_inches='tight', pad_inches=0.05)
+plt.legend(loc='upper right', frameon=False, ncol=2, columnspacing=0.2, handletextpad=0.2)
+plt.savefig("%s_v2.pdf" % outname, format="pdf", bbox_inches='tight', pad_inches=0.05)
 
--- algorithm_simulation/stats/beamer_statistics_collector.py
+++ algorithm_simulation/stats/beamer_statistics_collector.py
@@ -24,7 +24,7 @@ class StatisticsCollector(Collector):
         with open("./stats/report.csv",'ab+') as f:
             aa = csv.writer(f)
             aa.writerow([str(self.model), str(self.connection_target), str(self.endtime), str(self.server_number), "None",
-                         str(self.bucket_number),"None", str(self.total_con), "None", "None", str(self.max_imba), str(self.ave_imba)])
+                         str(self.bucket_number),"None", str(self.total_con), "None", "None", str(self.max_imba * 100), str(self.ave_imba * 100), "None"])
             f.close()
 
 class DynamicBeamerCollecter(Collector):
@@ -40,8 +40,8 @@ class DynamicBeamerCollecter(Collector):
         with open("./stats/report.csv", 'ab+') as f:
             aa = csv.writer(f)
             aa.writerow([str(self.model), str(self.connection_target), str(self.endtime), str(self.server_number), "None",
-                         str(self.bucket_number), str(self.Imbalance_threshold), str(self.total_con), str(self.unnecessary_move_count),
-                         str(float(self.unnecessary_move_count) / self.total_con), "None", "None", str((self.moving_bucket)/float(self.bucket_number))])
+                         str(self.bucket_number), str((self.Imbalance_threshold-1)*100), str(self.total_con), str(self.unnecessary_move_count),
+                         str((float(self.unnecessary_move_count) / self.total_con)*100), "None", "None", str((self.moving_bucket/float(self.bucket_number))*100)])
             f.close()
         self.moving_bucket = 0.0
 
@@ -61,5 +61,5 @@ class DipCollector(Collector):
             with open("./stats/report.csv",'ab+') as f:
                 aa = csv.writer(f)
                 aa.writerow([str(self.model), str(self.connection_target), str(self.endtime), str(self.server_number),
-                             str(self.rate_number),"None", "None", str(self.total_con),str(self.unnecessary_move_count),str(float(self.unnecessary_move_count)/self.total_con), "None", "None"])
+                             str(self.rate_number),"None", "None", str(self.total_con),str(self.unnecessary_move_count),str((float(self.unnecessary_move_count)/self.total_con)*100), "None", "None"])
                 f.close()
